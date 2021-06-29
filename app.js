259055ef7b7e8437fb1e128c89425dadd9260a10
Alice-Retz <alicegretz@gmail.com>
2021-06-29 17:10:53 -0500

    Added the quiz

--- app.js
+++ app.js
@@ -6,3 +6,31 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+import { isYes } from '../utils.js';
+
+const quizButton = document.getElementById('quiz-launch');
+const results = document.getElementById('results');
+
+quizButton.addEventListener('click', ()=>{
+    const confirmed = confirm('Would you like to start the quiz?');
+
+    if (confirmed){
+        console.log('continue the quiz');
+        const name = prompt('What is your name?');
+        const ans1 = prompt('Does the professor have a five foot vertical leap?');
+        const ans2 = prompt('Does the professor think a curtain is a solid wall?');
+        const ans3 = prompt('Does the professor understand how zippers work?');
+        const ans4 = prompt("Does the professor really want to jump into the oven every time it's on?");
+        const ans5 = prompt('Does the professor realize his actions have consequences?');
+        let score = 0 ;
+        if (isYes(ans1)) score++;
+        if (isYes(ans2)) score++;
+        if (isYes(ans3)) score++;
+        if (isYes(ans4)) score++;
+        if (isYes(ans5)) score++;
+        console.log('final score', score);
+        results.textContent = `${name} you got ${score} right!`;
+    } else {
+        console.log('user canceled');
+    }
+});
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,18 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { isYes } from '../utils.js';
+
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
-    //Arrange
-    // Set up your arguments and expectations
-    const expected = true;
-    
-    //Act 
-    // Call the function you're testing and set the result to a const
-    const actual = true;
+test('should return true for yes-y strings', expect => {
+    const yesString = isYes('yes');
+    const yString = isYes('y');
+    expect.equal(yesString, true);
+    expect.equal(yString, true);
+});
 
-    //Expect
-    // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+test('should return false for non yes-y strings', expect => {
+    const noString = isYes('no');
+    expect.equal(noString, false);
 });
