94a384ed79118c4c2929280a6b0c36b3a768d908
Alice-Retz <alicegretz@gmail.com>
2021-06-29 20:08:25 -0500

    Multiple result messages added.

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
-// import functions and grab DOM elements
-
-// initialize state
-
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
 import { isYes } from '../utils.js';
+const resultGood = document.getElementById('good-result');
+const resultBad = document.getElementById('bad-result');
 
 const quizButton = document.getElementById('quiz-launch');
 const results = document.getElementById('results');
@@ -28,10 +22,15 @@ quizButton.addEventListener('click', ()=>{
         if (isYes(ans3)) score++;
         if (isYes(ans4)) score++;
         if (isYes(ans5)) score++;
-        alert("You're )
+        alert("You're done! Let's see how you did.");
         console.log('final score', score);
-        results.textContent = `${name} you got ${score} right! That's ${Math.floor(score/5*100)%.}`;
-
+        if (score >= 4) {
+            resultGood.style.display = 'block';
+            return results.textContent = `${name}, you got ${score} right! That's ${Math.floor(score / 5 * 100)}%! You know that the professor hates bacon, and understands that if he whacks Kiki while she's sleeping he'll get squirt with water, he just doesn't care!`;
+        } else {
+            resultBad.style.display = 'block';
+            return results.textContent = `${name}, you got ${score} right. That's ${Math.floor(score / 5 * 100)}%. Maybe you should spend sometime looking at cross-eyed orange cats to study. Here, let me help:`;
+        }
     } else {
         console.log('user canceled');
     }
